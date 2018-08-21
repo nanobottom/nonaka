1999e30a586ed67f1b4beabd2888a732ae629516
nanobottom <nanobottom@icloud.com>
2018-08-21 20:43:02 +0900

    updated

--- bottle/client.py
+++ bottle/client.py
@@ -4,7 +4,25 @@ import configparser
 inifile = configparser.ConfigParser()
 inifile.read('./config.ini', 'UTF-8')
 def main():
-    blist = [1, 2, 3, 255, 77, 89, 90 ]
+    ascii_lst = []
+    ascii_str = 'http://test/'
+    for s in ascii_str:
+        ascii_lst.append(ord(s))
+    # 100bytesの内、埋まらなかった箇所に0x00を付加
+    zero_lst = [0] * (100 - len(ascii_lst))
+    ascii_lst.extend(zero_lst)
+
+    blist = [1, 2, 3, 255, 77, 89, 90]
+    blist.extend(ascii_lst)
+    count = 1
+    for i in blist:
+        if count % 16 == 0:
+            print('%02x \n' % i, end = '')
+        else:
+            print('%02x ' % i, end = '')
+            if len(blist) == count:
+                print('\n')
+        count += 1
     the_bytearray = bytearray(blist)
     response = requests.post(
         'http://' + inifile.get('settings', 'host')
