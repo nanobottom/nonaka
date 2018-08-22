1352812be2313e1ad27aef4fdb398b9df0617afe
nanobottom <nanobottom@icloud.com>
2018-08-22 22:16:31 +0900

    updated

--- bottle/test.py
+++
@@ -1,9 +0,0 @@
-
-import configparser
-from bottle import route, template, run
-inifile = configparser.ConfigParser()
-inifile.read('./config.ini', 'UTF-8')
-@route('/hello/<name>')
-def hello(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
-run(host = inifile.get('settings', 'host'), port = inifile.get('settings', 'port'))
