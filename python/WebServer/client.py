e161c7fb58f109ffab7ed85227952b9c326f8a16
nanobottom <nanobottom@icloud.com>
2018-08-26 06:48:00 +0900

    Updated

--- python/WebServer/client.py
+++ python/WebServer/client.py
@@ -7,10 +7,10 @@ inifile.read('./config.ini', 'UTF-8')
 def main():
     URL = 'http://' + inifile.get('settings', 'host') + ':' + inifile.get('settings', 'port') + '/post'
     web_data = WebData()
-    ascii_lst = web_data.str_to_ascii('http://localhost:8080/test',100) 
-    blist = [1, 2, 3, 255, 77, 89, 90]
-    blist.extend(ascii_lst)
-    the_bytearray = bytearray(blist)
+    testURL = web_data.str_to_ascii('http://localhost:8080/test',100) 
+    request_body = [1, 2, 3, 255, 77, 89, 90]
+    request_body.extend(testURL)
+    the_bytearray = bytearray(request_body)
     response = requests.post(URL, the_bytearray)
 
     #pprint.pprint(response.json())
