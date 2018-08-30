267b69842c53b1ab8eeb17128a91197ef7b55e7f
nanobottom <nanobottom@icloud.com>
2018-08-30 22:20:48 +0900

    updated

--- python/WebServer/main.py
+++ python/WebServer/main.py
@@ -4,7 +4,5 @@ import routes
 inifile = configparser.ConfigParser()
 inifile.read('./config.ini', 'UTF-8')
 
-print('host : {}'.format(inifile.get('settings', 'host')))
-print('port : {}'.format(inifile.get('settings', 'port')))
 if __name__ == '__main__':
     run(host = inifile.get('settings', 'host'), port = inifile.get('settings', 'port'))
