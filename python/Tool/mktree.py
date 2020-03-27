3ac4a91caa6b4def70bfda77ac8f39223e8a04b8
nanobottom <nanobottom@icloud.com>
2020-03-28 01:37:03 +0900

    updated

--- python/Tool/mktree.py
+++ python/Tool/mktree.py
@@ -5,11 +5,10 @@ from datetime import date
 class Tree():
 
     def __init__(self):
-        self.search_path = 'C:/cygwin64/home/nanob/git'
+        self.search_path = 'C:/cygwin64/home'
         self.tree = dict()
         self.tree_str = str()
         # 保存するファイル名を作成する
-        #pc_name = os.uname().nodename
         pc_name = platform.uname().node
         name = '野中'
         today = date.today().strftime('%Y%m%d')
@@ -36,50 +35,65 @@ class Tree():
         os.remove(self.filename)
 
     def mktree(self, path, tree, current_depth=0, tab=''):
-        files = tree[path][0]
-        dirs = tree[path][1]
-        if current_depth == 0:
-            # ルートを表示する
-            self.tree_str += path.rsplit(os.sep, 1)[-1]
-            self.tree_str += '\n'
-        # ファイルを表示する
-        if len(files) != 0:
-            for i in range(len(files)):
-                if i != len(files) - 1:
-                    self.tree_str += tab
-                    self.tree_str += '┣━'
-                    self.tree_str += files[i]
-                    self.tree_str += '\n'
-                # 現在のファイルが最後の場合
-                else:
-                    if len(dirs) == 0:
-                        self.tree_str += tab
-                        self.tree_str += '┗━'
-                        self.tree_str += files[i]
-                        self.tree_str += '\n'
-                    else:
+        # ゴミ箱を除く
+        if path != os.path.join(self.search_path, '$RECYCLE.BIN'):
+            files = list()
+            dirs = list()
+            dir_list = os.listdir(path)
+            for current_dir in dir_list:
+                # 隠しフォルダを除く
+                if not current_dir.startswith('.'):
+                    # ディレクトリか確認する
+                    if os.path.isdir(os.path.join(path, current_dir)):
+                        dirs.append(current_dir)
+                    # ファイルか確認する
+                    elif os.path.isfile(os.path.join(path, current_dir)):
+                        files.append(current_dir)
+
+            #files = tree[path][0]
+            #dirs = tree[path][1]
+            if current_depth == 0:
+                # ルートを表示する
+                self.tree_str += path.rsplit(os.sep, 1)[-1]
+                self.tree_str += '\n'
+            # ファイルを表示する
+            if len(files) != 0:
+                for i in range(len(files)):
+                    if i != len(files) - 1:
                         self.tree_str += tab
                         self.tree_str += '┣━'
                         self.tree_str += files[i]
                         self.tree_str += '\n'
+                    # 現在のファイルが最後の場合
+                    else:
+                        if len(dirs) == 0:
+                            self.tree_str += tab
+                            self.tree_str += '┗━'
+                            self.tree_str += files[i]
+                            self.tree_str += '\n'
+                        else:
+                            self.tree_str += tab
+                            self.tree_str += '┣━'
+                            self.tree_str += files[i]
+                            self.tree_str += '\n'
 
-        # ディレクトリを表示する
-        if len(dirs) != 0:
-            for i in range(len(dirs)):
-                if i != len(dirs) - 1:
-                    self.tree_str += tab
-                    self.tree_str += '┣━'
-                    self.tree_str += dirs[i]
-                    self.tree_str += '\n'
-                    self.mktree(os.path.join(path, dirs[i]),\
-                        tree, current_depth=current_depth+1, tab=tab+'┃\t')
-                else:
-                    self.tree_str += tab
-                    self.tree_str += '┗━'
-                    self.tree_str += dirs[i]
-                    self.tree_str += '\n'
-                    self.mktree(os.path.join(path, dirs[i]),\
-                       tree, current_depth=current_depth+1, tab=tab+' \t')
+            # ディレクトリを表示する
+            if len(dirs) != 0:
+                for i in range(len(dirs)):
+                    if i != len(dirs) - 1:
+                        self.tree_str += tab
+                        self.tree_str += '┣━'
+                        self.tree_str += dirs[i]
+                        self.tree_str += '\n'
+                        self.mktree(os.path.join(path, dirs[i]),\
+                            tree, current_depth=current_depth+1, tab=tab+'┃\t')
+                    else:
+                        self.tree_str += tab
+                        self.tree_str += '┗━'
+                        self.tree_str += dirs[i]
+                        self.tree_str += '\n'
+                        self.mktree(os.path.join(path, dirs[i]),\
+                           tree, current_depth=current_depth+1, tab=tab+' \t')
 
 if __name__ == '__main__':
     tree = Tree()
