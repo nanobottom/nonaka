32a4f85e31918355d7eed6d4dadb44b4887e30cc
nanobottom <nanobottom@icloud.com>
2020-04-04 23:14:08 +0900

    updated

--- python/Excel/Xlsxwriter/mk_attendance_book/scraping_name.py
+++ python/Excel/Xlsxwriter/mk_attendance_book/scraping_name.py
@@ -1,7 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+proxies = {
+    'http':'',
+    'https':''
+}
 SITE_URL = ''
-response = requests.get(SITE_URL)
+response = requests.get(SITE_URL, proxies=proxies)
 soup = BeautifulSoup(response.content, "lxml")
 
